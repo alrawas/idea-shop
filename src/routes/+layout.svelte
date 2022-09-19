e1c07f8ac3b07de3959f5a2d26e9f9b66b4d1be8
alrawas <alrawas.abed@gmail.com>
2022-09-19 23:58:57 +0300

    added picocss and feat product section in home

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,54 @@
+<script>
+	import '$lib/assets/css/pico.min.css';
+</script>
+
+<div class="nav-wrapper">
+	<nav class="container">
+		<ul>
+			<div class="logo-wrapper">
+				<img class="logo" alt="logo" src="/images/logo_low_res.jpg" />
+			</div>
+      <span style="margin-left: 1rem;">IDEA Gift Shop</span>
+		</ul>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/Shop">Shop</a></li>
+			<li><a href="/about">About</a></li>
+		</ul>
+	</nav>
+</div>
+
+<div class="hero" />
+
+<main class="container">
+	<slot />
+</main>
+
+<div class="footer-wrapper">
+	<footer class="container">hi</footer>
+</div>
+
+<style>
+	.footer-wrapper {
+		padding: 1rem;
+		background-color: gray;
+		color: whitesmoke;
+	}
+	.nav-wrapper {
+		padding: 0.5rem 0.25rem;
+		background-color: aliceblue;
+	}
+	main {
+		padding: 2rem 1rem;
+	}
+	.hero {
+		background-color: #394046;
+		background-image: url('assets/sascha-eremin-DNQ-M93tHmA-unsplash-3000x1000.jpg');
+		background-position: center;
+		background-size: cover;
+		height: 20vh;
+	}
+  .logo {
+    height: 75px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to IDEA Gift Shop</h1>
+<h2>Featured Products</h2>
+<div class="grid">
+	<div>
+		<article><header><h3>Featured Product 1</h3></header></article>
+	</div>
+	<div>
+		<article><header><h3>Featured Product 2</h3></header></article>
+	</div>
+  <div>
+		<article><header><h3>Featured Product 3</h3></header></article>
+	</div>
+</div>
+<div class="grid">
+	<div>
+		<article><header><h3>Featured Product 4</h3></header></article>
+	</div>
+  <div>
+		<article><header><h3>Featured Product 5</h3></header></article>
+	</div>
+  <div>
+		<article><header><h3>Featured Product 6</h3></header></article>
+	</div>
+</div>
